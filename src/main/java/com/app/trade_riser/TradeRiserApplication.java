3c50fe0685d994673838aa4073330e7869e16eb4
Roman Micuda <roman.micuda@outlook.com>
2025-08-02 10:26:11 +0200

    chore: add initial project structure with Spring Boot configuration and PostgreSQL setup

---
+++ src/main/java/com/app/trade_riser/TradeRiserApplication.java
@@ -0,0 +1,13 @@
+package com.app.trade_riser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeRiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeRiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/trade_riser/TradeRiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.trade_riser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeRiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
