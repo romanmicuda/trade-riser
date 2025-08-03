0842ed177308fd4bba57508b89e9164c2309f064
Roman Micuda <roman.micuda@outlook.com>
2025-08-03 19:53:28 +0200

    TR-1: feat: add Dashboard and NotFound components; update routing in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import { SignIn } from './components/SignIn';
 import { SignUp } from './components/SignUp';
+import Dashboard from './components/Dashboard';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
